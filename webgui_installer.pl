f14cec82ae2824d6dcc9e7429361fe5abf87d9f5
Scott Walters <scott@slowass.net>
2012-08-20 23:16:30 -0400

    redraw after system'ing out to apt-get and percona's config curses UI

--- webgui_installer.pl
+++ webgui_installer.pl
@@ -729,6 +729,8 @@ if( $mysqld_safe_path) {
         # run( $sudo_command . 'apt-get install -y -q percona-server-server-5.5 libmysqlclient18-dev' ); # no can do; Debian fires up a curses UI and asks for a root password to set, even with 'quiet' set, so just shell out
         system( "echo $sudo_password | $sudo_command  apt-get install -y percona-server-server-5.5 libmysqlclient18-dev" ); 
 
+         main_win();  update();    # redraw
+
         # go look for mysqld again now that it should be installed
 
         goto scan_for_mysqld;
