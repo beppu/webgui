4cb57e107d9c4a508659f47f3abe849a36103be2
Scott Walters <scott@slowass.net>
2012-11-12 17:04:34 -0500

    development cleanup.sh removing more things -- config files in particular

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,5 @@
 rm -rf /data
+rm -f /etc/nginx/conf.d/webgui8.conf
+rm -f /etc/rc.d/*/*webgui8
 mysql --user=root --password=Nyklm6 -e 'drop database www_example_com;'
 # apt-get remove -y percona-server-server-5.5
