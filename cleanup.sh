a610e640d5556acc1256240243e7b86e6a9f4b7b
Scott Walters <scott@slowass.net>
2012-11-12 19:17:52 -0500

    kill starman processes in cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,6 @@
 rm -rf /data
 rm -f /etc/nginx/conf.d/webgui8.conf
 rm -f /etc/rc.d/*/*webgui8
+killall starman
 mysql --user=root --password=Nyklm6 -e 'drop database www_example_com;'
 # apt-get remove -y percona-server-server-5.5
