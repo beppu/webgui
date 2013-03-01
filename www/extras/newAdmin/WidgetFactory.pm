febdd6c75ffb9f0408cfa9bdb1355b589d4fb81e
root <root@webgui.dbash.com>
2013-02-28 21:57:59 -0600

    Datatable working off this widget

--- www/extras/newAdmin/WidgetFactory.pm
+++ www/extras/newAdmin/WidgetFactory.pm
@@ -19,9 +19,9 @@ Package WebGUI::Macro::WidgetFactory
 
 =head1 DESCRIPTION
 
-Handy example code for starting a new Macro when you have to start from scratch.
+Handy Macro to create WebWidgets, it can take the difficulty out of ...
 
-=head2 process( $session, [@other_options] )
+=head2 process( $session, widget, [@other_options] )
 
 The main macro class, Macro.pm, will call this subroutine and pass it
 
@@ -44,20 +44,70 @@ to validate user input.
 #-------------------------------------------------------------------
 sub process {
    my $session = shift;
-   my $columnConfig = [
-     { column => "userId",       title => "UserId" },
-     { column => "lastIP",       title => "Last Ip" },
-     { column => "lastPageView", title => "Last View" },
-     { column => "username",     title => "Username" },
-     { column => "expires",      title => "Expires" },
-	  { column => "sessionId",    title => "Kill Session", checkbox => "1", cssClass => "killSession", name => 'killSession' }
-   ];
+	my $widget = shift;
+	my @tableConfig =  @_;
 	
-   my $datatable = WebGUI::Form::DataTablesNet->new(
-      $session, { columnConfig => $columnConfig, noScript => 1, restDataUrl => "/?op=viewActiveSessions", id => "sessionsDatatable" }
-   );
+	my $config = {};
+	my $foundRawConfig = undef;	
+	my $columnConfig = [];
+	my $configSettings = [];
+	while( my $option = shift( @tableConfig ) ){
+		my $strippedOption = WebGUI::HTML::filter( $option, 'all' );
+		if ( $strippedOption =~ m/config:/ ){
+		   $foundRawConfig = 1;
+			$config->{config} = createHashRef( $strippedOption )->{config};
+			
+		}else{
+		   if ( $foundRawConfig ){
+            $config->{config} .= $strippedOption;
+			}else{
+		      if( $strippedOption =~ m/column:/ ){
+					 push( @{ $columnConfig }, createHashRef( $strippedOption ) );
+				}else{
+					 push(@{ $configSettings }, createHashRef( $strippedOption ) );
+				}
+			}
+		}
+	}
+	
+   $config->{columnConfig} = $columnConfig;
+	
+   foreach my $singleConfig ( @{ $configSettings } ){
+      for my $key ( keys( %{ $singleConfig } ) ){
+         $config->{ $key } = $singleConfig->{ $key };
+      }
+   }
+
+#   my $columnConfig = [
+#     { column => "userId",       title => "UserId" },
+#     { column => "lastIP",       title => "Last Ip" },
+#     { column => "lastPageView", title => "Last View" },
+#     { column => "username",     title => "Username" },
+#     { column => "expires",      title => "Expires" },
+#	  { column => "sessionId",    title => "Kill Session", checkbox => "1", cssClass => "killSession", name => 'killSession' }
+#   ];
+	
+   #my $datatable = WebGUI::Form::DataTablesNet->new(
+   #   $session, { config => $rawConfig, columnConfig => $columnConfig, noScript => 1, restDataUrl => "/?op=viewActiveSessions", id => "sessionsDatatable" }
+   #);
+
+   my $datatable = WebGUI::Form::DataTablesNet->new( $session, $config );
    return $datatable->toHtml();
 
+}
+
+sub createHashRef{
+	 my $rawData = shift;
+    $rawData =~ s/\{//g;
+    $rawData =~ s/\}//g;
+    my @options = split(';', $rawData);
+    my $usableData = {};
+    foreach my $option ( @options ){
+        $option =~ s/'//g;
+    	  my ( $key, $value ) = split(':', $option);
+    	  $usableData->{ $key } = $value;
+    }
+	 return $usableData;
 }
 
 1;
