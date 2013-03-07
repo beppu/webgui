5c9b4fb0478151acc1679bf20b0d12f0440b4599
Daniel Maldonado <danny_mk@yahoo.com>
2013-03-07 11:49:42 -0500

    Added dynamically built menu code

---
+++ www/extras/newAdmin/js/application.js
@@ -0,0 +1,133 @@
+// Main webgui configuration object for scripts.  This can be put into a template and
+//   retrieved from the WebGUI config system
+WEBGUI_ADMIN_CONFIG = {
+   "template" : {
+      "path":"/js/templates/"
+   }
+};
+
+/*
+ * Must create these types of menu items to be added to the dynamic menus.
+ */
+var MenuDynamic = can.Construct({},{
+   init:function(href,link,title){
+      this.id    = Math.random();
+      this.href  = href;
+      this.link  = link; 
+      this.title = title;
+   },
+   equals:function(menuItem){
+      if ( menuItem instanceof MenuDynamic ){
+         if ( this.id === menuItem.id ){
+            return true;
+         }else{
+            return false;
+         }
+
+      }else{
+         return false;
+      }
+   },
+   id:function(){ return this.id; },
+   href:function(){ return this.href; },
+   link:function(){ return this.link; },
+   title:function(){ return this.title; }
+});
+
+/* Menu has a few options.
+ * id = The UL tag id
+ * view = the ejs template used to generate the menu, see default template crumbTrail.ejs
+ * source = the json datasource
+ *    should be in the form [{ "id": "1", "title": "...", "href": "/?op=1", "link": "Home"  },...]
+ *    will create an anchor tag: 
+ *    <ul [id=...] class="webgui_breadcrumb_container">
+ *       <li class="webgui_breadcrumb_item"><a href="/?op=1" title="">Home</a></li>
+ *       ...
+ *    </ul>
+*/
+var MenuFromJson = can.Control({
+   defaults: {
+      levels: 5,
+      view: WEBGUI_ADMIN_CONFIG.template.path + 'crumbTrail.ejs' 
+   }
+},{
+   'init': function( element , options ) {
+      // Build the model that actually retrieves the menus, the items should be filtered and 
+      //   sorted on the server
+      var MenuItems = can.Model({ findAll : 'GET ' + options.source + '?levels=' + options.levels }, {});
+      // Retrieve the menu items, render them in the container using the assigned template 
+      MenuItems.findAll({}, function( items ) {
+         $( element ).html( can.view( options.view, { items:items, id:options.id } ) );
+      });
+   },
+   "li click" : function(li, event){
+      event.preventDefault(); // suppress the click event
+      event.stopPropagation();
+      var link  = li.data('link');
+      var divId = this.options.divId; 
+      renderPage( divId, link.href );
+   }
+});
+
+/*
+ *  Creates a dynamic crumbtrail menu in the passed div
+ */
+var CrumbTrailMenu = can.Control({
+   defaults: {
+      levels: 5,
+      view: WEBGUI_ADMIN_CONFIG.template.path + 'crumbTrail.ejs' 
+   }      
+
+},{
+   'init': function( element , options ) {
+      if ( options.items != null ){ // do NOT change!!!
+         this.menuItems = options.items;
+      }else{
+         this.menuItems = new can.Observe.List([]);          
+      }
+
+
+      if ( options.id !== null ){
+         this.id = options.id;
+      }
+      element.html( can.view( options.view, { id:this.id, items:this.menuItems } ) );
+   },
+   'li click':function(li, event){
+      event.preventDefault();
+      var link  = li.data('link');
+      // remove everything after the clicked item
+      var instancePosition = 0;
+      for( var index = 0; index < this.menuItems.length; index++ ){// menu should not be empty
+         if ( link.equals( this.menuItems[index] ) ){
+            instancePosition = index + 1;
+            break;//we are out of here!
+         }
+      }
+      while( instancePosition > 0 && this.menuItems.length > instancePosition ){
+         this.menuItems.pop();
+      }
+
+      var divId = this.options.divId; // Where we display the contents of the requested path
+      renderPage( divId, link.href );
+
+   },
+   add:function(menuItem){
+      if ( menuItem instanceof MenuDynamic ){
+         this.menuItems.push(menuItem);
+      }else{
+         throw "You must pass an instance of: MenuDynamic";//::i18n::
+      }
+   },
+   count:function(){
+      return this.menuItems.length;
+   }
+});
+
+// given a page and a div id render the contents
+function renderPage(targetDiv, operation){
+   if ( $(targetDiv).length > 0 ){
+      $(targetDiv).load( operation );
+   }else{
+      throw "You must provide a target to render the content of: " + operation;// ::i18n::
+   }
+}
