458db77f02f5f102f7161ad1564b476922fcc7e0
Daniel Maldonado <danny_mk@yahoo.com>
2013-03-21 17:20:17 -0400

    Updated from client app

--- www/extras/newAdmin/js/AdminApp.js
+++ www/extras/newAdmin/js/AdminApp.js
@@ -21,9 +21,9 @@ requirejs.config({
          template: {
             path: "/extras/newAdmin/js/templates/"
          },
-         jsonSourceServer: "http://webgui.dbash.com:8900",
-         jsonp:true,
-         otherHelp:'/json/help.json',
+         jsonSourceServer: "/",
+         jsonp:false,
+         otherHelp:'/extras/newAdmin/json/help.json',
          tooltips: true,
          messageTag: "#messages",
          messageTemplate: "webgui-ajax-message-template.ejs"
